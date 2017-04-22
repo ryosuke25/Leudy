d08ecf973dbbdbc7b08befc0620b885ed030230d
=ryosuke25 <keisukefd25@gmail.com>
2017-04-22 17:39:08 -0400

    se ah guardado lo de php nuevo

--- index.php
+++ index.php
@@ -2,8 +2,17 @@
  <html>
  <head>
  	<title>no se</title>
+ 	<link rel="stylesheet" type="text/css" href="css/estilo.css">
  </head>
  <body>
+
+ <?php 
+$persona = new persona();
+
+
+
+
+  ?>
  <h1>Madeline</h1>
  <p>hrp company</p>
 
